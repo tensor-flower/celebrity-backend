0a8c8ecfdcbc8fc0b0f8a4bdc2de83f91d03a570
tensor-flower <wanqiguo2@gmail.com>
2019-03-27 22:53:47 +0800

    db config

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const bcrypt = require('bcrypt');
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'guowanqi',
-    password : '',
-    database : 'brain'
+    connectionString : 'process.env.DATABASE_URL',
+    ssl: true
   }
 });
 
